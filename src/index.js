f683b3ded5de30c46cefdb734266088bc82f874f
Jeya <brintina@gmail.com>
2018-09-26 13:12:25 +0800

    submit old stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
+
 import './index.css';
-import App from './App';
+import Login from './step4/Login';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Reducer
+function counter(state = { count: 0 }, action) {
+    const count = state.count
+    switch (action.type) {
+        case 'increase':
+            return { count: count + 1 }
+        default:
+            return state
+    }
+}
+
+// Store
+const store = createStore(counter)
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <Login />
+    </div>,
+
+    document.getElementById('root')
+)
 registerServiceWorker();
---
+++ src/step4/Information.js
@@ -0,0 +1,5 @@
+'import Login from "./Login";
+import Information from './information'
+
+<route
+
